2e8a6664062b71c804cbebf852808fc950733b7a
unknown <2294387616@qq.com>
2024-02-13 02:17:27 +0000

    Update pacman.bat

--- pacman.bat
+++ pacman.bat
@@ -4,4 +4,6 @@ cd /d %~dp0
 
 processing-java --force --sketch="%cd%\main" --run
 
-:: processing-java --force --sketch="%cd%\main" --output="%cd%\out" --run
+@REM processing-java --force --sketch="%cd%\main" --output="%cd%\out" --run
+
+@REM processing-java --force --sketch="%cd%\main" --export --platform=windows --output="%cd%\out"
