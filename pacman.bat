e9253156c707e2cb213cdb1252601e5ec91bfdd6
ad23091 <157363040+ad23091@users.noreply.github.com>
2024-05-01 21:46:21 +0100

    Delete pacman.bat

--- pacman.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-cd /d %~dp0
-
-processing-java --force --sketch="%cd%\main" --run
-
-@REM processing-java --force --sketch="%cd%\main" --output="%cd%\out" --run
-
-@REM processing-java --force --sketch="%cd%\main" --export --platform=windows --output="%cd%\out"
